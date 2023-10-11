13bf1443cd72cb284e74921d9cac8b3e5077872c
Carlos Manuel Alvarez Lopez <carlosmal1502@gmail.com>
2023-10-10 23:22:59 -0600

    Dispositivos Electronicos

--- Practica/Dispositivos/DispositivosElectronicos/DispositivoElectronico.java
+++ Practica/Dispositivos/DispositivosElectronicos/DispositivoElectronico.java
@@ -1,3 +1,33 @@
 package Dispositivos.DispositivosElectronicos;
 
-public class DispositivoElectronico{}
+public class DispositivoElectronico{
+
+    private int año;
+    private boolean bateriaInterna;
+
+    public DispositivoElectronico(int año, boolean bateriaInterna){
+        this.año = año;
+        this.bateriaInterna = bateriaInterna;
+    }
+
+    public void setBateriaInterna(boolean bateriaInterna){
+        this.bateriaInterna = bateriaInterna;
+    }
+
+    protected void setAño(int año){
+        this.año = año;
+    }
+
+    protected boolean getBateriaInterna(){
+        return bateriaInterna;
+    }
+
+    protected int getAño(){
+        return año;
+    }
+
+    protected String getClassName(){
+        return "DispositivoElectronico";
+    }
+
+}
--- Practica/Dispositivos/DispositivosElectronicos/Television.java
+++ Practica/Dispositivos/DispositivosElectronicos/Television.java
@@ -1,3 +1,44 @@
 package Dispositivos.DispositivosElectronicos;
 
-public class Television extends DispositivoElectronico{}
+public class Television extends DispositivoElectronico{
+
+    private String modelo;
+    private int resolucion;
+    private String audio;
+
+    public Television(int año, boolean bateriaInterna, String modelo, int resolucion, String audio){
+        super(año, bateriaInterna);
+        this.modelo = modelo;
+        this.resolucion = resolucion;
+        this.audio = audio;
+    }
+
+    protected void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    
+    protected void setResolucion(int resolucion){
+        this.resolucion = resolucion;
+    }
+    
+    protected void setAudio(String audio){
+        this.audio = audio;
+    }
+
+    protected String getModelo(){
+        return modelo;
+    }
+
+    protected int getResolucion(){
+        return resolucion;
+    }
+    
+    protected String getAudio(){
+        return audio;
+    }
+
+    protected String getClassName(){
+        return "Television";
+    }
+
+}
