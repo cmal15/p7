fa3a79b863704e33fa179f89ec7e488304d85054
Isaias1111 <112528330+Isaias1111@users.noreply.github.com>
2023-10-11 00:02:42 -0600

    Update PC.java

--- Practica/Dispositivos/Computadoras/PC.java
+++ Practica/Dispositivos/Computadoras/PC.java
@@ -1,3 +1,30 @@
 package Dispositivos.Computadoras;
 
-public class PC extends Computadora{}
+public class PC extends Computadora{
+      protected boolean correFriFayer;
+    public PC(String modelo, String marca, boolean correFriFayer){
+        super(modelo,marca);
+        this.correFriFayer=correFriFayer;
+    }
+    protected static PC nuevo(Scanner sc){
+        String modelo;
+        String marca;
+        boolean correFriFayer;
+        System.out.print("Introduzca el modelo: ");
+        modelo = sc.nextLine();
+        System.out.print("Introduzca la marca: ");
+        marca = sc.nextLine();
+        System.out.print("Puedes jugar FreeFire en esta compu(true/false): ");
+        correFriFayer = sc.nextBoolean();
+        return new PC(modelo,marca,correFriFayer);
+    }
+    protected void setcorreFriFayer(boolean correFriFayer){
+        this.correFriFayer=correFriFayer;
+    }
+    protected boolean getCorreFriFayer(){
+        return correFriFayer;
+    }
+    protected String getClassName(){
+        return "PC";
+    }
+}
