476481a97e09f9aba972f89c14f9268630a2ebd0
Isaias1111 <112528330+Isaias1111@users.noreply.github.com>
2023-10-11 00:01:47 -0600

    Update Computadora.java

--- Practica/Dispositivos/Computadoras/Computadora.java
+++ Practica/Dispositivos/Computadoras/Computadora.java
@@ -2,48 +2,41 @@ package Dispositivos.Computadoras;
 
 import Dispositivos.DispositivosElectronicos.DispositivoElectronico;
 
-public class Computadora extends DispositivoElectronico{    protected String modelo;
-    protected String marca;
-    protected float precio;
-
-    public Computadora(String modelo, String marca, float precio){
-        this.modelo = modelo;
-        this.marca = marca;
-        this.precio = precio;
+public class Computadora extends DispositivoElectronico{   
+    protected int peso;
+    protected int duracionBateria;
+    public Laptop(String modelo, String marca, int peso, int duracionBateria){
+        super(modelo,marca);
+        this.peso=peso;
+        this.duracionBateria=duracionBateria;
     }
-
-    public static Computadora nuevo(Scanner sc){
+    protected static Laptop nuevo(Scanner sc){
         String modelo;
         String marca;
-        float precio;
+        int peso,duracionBateria;
         System.out.print("Introduzca el modelo: ");
         modelo = sc.nextLine();
         System.out.print("Introduzca la marca: ");
         marca = sc.nextLine();
-        System.out.print("Introduzca el precio: ");
-        precio = sc.nextFloat();
-        return new Computadora(modelo,marca,precio);
+        System.out.print("Introduzca el peso del modelo: ");
+        peso = sc.nextInt();
+        System.out.print("Introduzca la duracion de la bateria: ");
+        duracionBateria = sc.nextInt();
+        return new Laptop(modelo,marca,peso,duracionBateria);
     }
-
-    public float getPrecio(){
-        return precio;
+    protected void setPeso(int peso){
+        this.peso=peso;
     }
-    public String getModelo(){
-        return modelo;
+    protected int getPeso(){
+        return peso;
     }
-    public String getMarca(){
-        return marca;
+    protected void setduracionBateria(int duracionBateria){
+        this.duracionBateria=duracionBateria;
     }
-    public void setPrecio(int precio){
-        this.precio =  precio;
+    protected int getduracionBateria(){
+        return duracionBateria;
     }
-    public void setModelo(String modelo){
-        this.modelo = modelo;
-    }
-    public void setMarca(String modelo){
-        this.marca = marca;
-    }
-    public String getClassName(){
-        return "Computadora";
+    protected String getClassName(){
+        return "LapTop";
     }
   }
