09d44f7fb0cf450515843b2003cb0d18dfd1d13d
Isaias1111 <112528330+Isaias1111@users.noreply.github.com>
2023-10-10 23:21:09 -0600

    Update Socio.java

--- Practica/Personas/Socio.java
+++ Practica/Personas/Socio.java
@@ -2,11 +2,11 @@ package Personas;
 import java.util.Scanner;
 public class Socio extends Comprador{
     protected String codigoDeConvenio;
-    public Socio(String nombre, String apellido, int numeroDeCuenta, String codigoDeConvenio){
+    protected Socio(String nombre, String apellido, int numeroDeCuenta, String codigoDeConvenio){
         super(nombre,apellido,numeroDeCuenta);
         this.codigoDeConvenio=codigoDeConvenio;
     }
-    public static Socio nuevo(Scanner sc){
+    protected static Socio nuevo(Scanner sc){
         int numeroDeCuenta;
         String nombre;
         String apellido;
@@ -21,10 +21,10 @@ public class Socio extends Comprador{
         codigoDeConvenio = sc.nextLine();
         return new Socio(nombre,apellido,numeroDeCuenta,codigoDeConvenio);
     }
-    public void setCodigoDeConvenio(String codigoDeConvenio){
+    protected void setCodigoDeConvenio(String codigoDeConvenio){
         this.codigoDeConvenio=codigoDeConvenio;
     }
-    public String getCodigoDeConvenio(){
+    protected String getCodigoDeConvenio(){
         return codigoDeConvenio;
     }
 }
