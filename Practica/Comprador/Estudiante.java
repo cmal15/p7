fa4d475d930fe8fcb7b6df1c3723b0726e0b29cf
Isaias1111 <112528330+Isaias1111@users.noreply.github.com>
2023-10-10 22:13:10 -0600

    Update Estudiante.java

--- Practica/Comprador/Estudiante.java
+++ Practica/Comprador/Estudiante.java
@@ -1,1 +1,30 @@
-public class Estudiante extends Comprador{}
+package Comprador;
+import java.util.Scanner;
+public class Estudiante extends Comprador{
+    protected String nivelDeEstudios;
+    public Estudiante(String nombre, String apellido, int numeroDeCuenta, String nivelDeEstudios){
+        super(nombre,apellido,numeroDeCuenta);
+        this.nivelDeEstudios=nivelDeEstudios;
+    }
+    public static Estudiante nuevo(Scanner sc){
+        int numeroDeCuenta;
+        String nombre;
+        String apellido;
+        String nivelDeEstudios;
+        System.out.print("Introduzca numero de cuenta: ");
+        numeroDeCuenta = sc.nextInt();
+        System.out.print("Introduzca nombre: ");
+        nombre = sc.nextLine();
+        System.out.print("Introduzca apellido: ");
+        apellido = sc.nextLine();
+        System.out.print("Introduzca cual es el nivel de estudios del estudiante: ");
+        nivelDeEstudios = sc.nextLine();
+        return new Estudiante(nombre,apellido,numeroDeCuenta,nivelDeEstudios);
+    }
+    public void setNivelDeEstudios(int numeroDeCuenta){
+        this.nivelDeEstudios=nivelDeEstudios;
+    }
+    public String getNivelDeEstudios(){
+        return nivelDeEstudios;
+    }
+}
