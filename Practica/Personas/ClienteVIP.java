599cefc3e40d6dcc25ca06a5a9bfbe7993c94abf
Isaias1111 <112528330+Isaias1111@users.noreply.github.com>
2023-10-12 22:35:50 -0600

    VIP

--- Practica/Personas/ClienteVIP.java
+++ Practica/Personas/ClienteVIP.java
@@ -3,19 +3,15 @@ import Dispositivos.Computadoras.*;
 import Dispositivos.DispositivosElectronicos.*;
 import Dispositivos.DispositivosMoviles.*;
 import java.util.Scanner;
+import java.util.LinkedList;
 
 public class ClienteVIP extends Comprador{
     public ClienteVIP(){
-        smartphones = new LinkedList<>();
-        tablets = new LinkedList<>();
-        televisiones = new LinkedList<>();
-        laptops = new LinkedList<>();
-        pcs = new LinkedList<>();
-        public LinkedList<Smartphone> smartphones;
-        public LinkedList<Tablet> tablets;
-        public LinkedList<Television> televisiones;
-        public LinkedList<Laptop> laptops;
-        public LinkedList<PC> pcs;
+        LinkedList<Smartphone> smartphones= new LinkedList<>();
+        LinkedList<Tablet> tablets= new LinkedList<>();
+        LinkedList<Television> televisiones= new LinkedList<>();
+        LinkedList<Laptop> laptops= new LinkedList<>();
+        LinkedList<PC> pcs= new LinkedList<>();
         Smartphone smartphone=new Smartphone(2023, 25000.0, "Apple", "Iphone", true, "T-mobile","ilimitado","Proteccion Max", "alta");
         smartphones.add(smarthpone);
         Tablet tablet=new Tablet(2023,30000.0,"Apple","Ipadpro",true,true)
