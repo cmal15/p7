48616de4fb6034a03531c554bbe88d3a9b177bd9
Carlos Manuel Alvarez Lopez <carlosmal1502@gmail.com>
2023-10-12 23:43:56 -0600

    a

--- Practica/Dispositivos/DispositivosMoviles/Smartphone.java
+++ Practica/Dispositivos/DispositivosMoviles/Smartphone.java
@@ -7,7 +7,7 @@ public class Smartphone extends Celular{
     private String proteccion;
     private String gama;
 
-    public Smartphone(int año, double precio, String marca, String modelo, int bateriaInterna,String companiaTelefonica,String planDeDatos, String proteccion, String gama){
+    public Smartphone(int año, double precio, String marca, String modelo, boolean bateriaInterna,String companiaTelefonica,String planDeDatos, String proteccion, String gama){
         super(año, precio, marca, modelo, bateriaInterna, companiaTelefonica, planDeDatos);
         this.proteccion = proteccion;
         this.gama = gama;
