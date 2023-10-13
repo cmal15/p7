6281aaec64223909e4b19707eead98bf32ea2560
Isaias1111 <112528330+Isaias1111@users.noreply.github.com>
2023-10-12 23:41:49 -0600

    a

--- Practica/Personas/Estudiante.java
+++ Practica/Personas/Estudiante.java
@@ -3,33 +3,28 @@ package Personas;
 import java.util.Scanner;
 
 public class Estudiante extends Comprador{
-    protected String nivelDeEstudios;
-    protected Estudiante(String nombre, String apellido, int numeroDeCuenta, String nivelDeEstudios){
-        super(nombre,apellido,numeroDeCuenta);
-        this.nivelDeEstudios=nivelDeEstudios;
+    public int descuento;
+    public Estudiante(Comprador comprador){
+        super(comprador.getSmartphones(),comprador.getTablets(),comprador.getTelevisions(),comprador.getLaptopss(),comprador.getPcss())
+        descuento=10;
     }
-
-    public static Estudiante nuevo(Scanner sc){
-        int numeroDeCuenta;
-        String nombre;
-        String apellido;
-        String nivelDeEstudios;
-        System.out.print("Introduzca numero de cuenta: ");
-        numeroDeCuenta = sc.nextInt();
-        System.out.print("Introduzca nombre: ");
-        nombre = sc.nextLine();
-        System.out.print("Introduzca apellido: ");
-        apellido = sc.nextLine();
-        System.out.print("Introduzca cual es el nivel de estudios del estudiante: ");
-        nivelDeEstudios = sc.nextLine();
-        return new Estudiante(nombre,apellido,numeroDeCuenta,nivelDeEstudios);
-    }
-    
-    protected void setNivelDeEstudios(String nivelDeEstudios){
-        this.nivelDeEstudios=nivelDeEstudios;
-    }
-    
-    protected String getNivelDeEstudios(){
-        return nivelDeEstudios;
+    public static Comprador iniciarEstudiante(){
+        LinkedList<Smartphone> smartphones= new LinkedList<>();
+        LinkedList<Tablet> tablets= new LinkedList<>();
+        LinkedList<Television> televisiones= new LinkedList<>();
+        LinkedList<Laptop> laptops= new LinkedList<>();
+        LinkedList<PC> pcs= new LinkedList<>();
+        Smartphone smartphone=new Smartphone(2015, 8000.0, "Xiomi", "Plus 30", true, "telcel","Basico","Sin proteccion", "media");
+        smartphones.add(smartphone);
+        Tablet tablet=new Tablet(2014,7000.0,"Xiaomi","Tablet z",true,true);
+        tablets.add(tablet);
+        Television tele=new Television(2015, 5000.00,"Samsung TV", 1080, "Estereo");
+        televisiones.add(tele);
+        Laptop laptop=new Laptop(2018,7000.0, "Notebook a", "Xiaomi", 10, 400);
+        laptops.add(laptop);
+        PC pc=new PC(2016, 8000.0, "Pc armada", "sin marca", true);
+        pcs.add(pc);
+        comprador=new Comprador(smartphones,tablets,televisiones,laptops,pcs);
+        return comprador;
     }
 }
--- Practica/Personas/Socio.java
+++ Practica/Personas/Socio.java
@@ -1,33 +1,26 @@
 package Personas;
 import java.util.Scanner;
 public class Socio extends Comprador{
-    protected String codigoDeConvenio;
-    protected Socio(String nombre, String apellido, int numeroDeCuenta, String codigoDeConvenio){
-        super(nombre,apellido,numeroDeCuenta);
-        this.codigoDeConvenio=codigoDeConvenio;
+    public Socio(Comprador comprador){
+        super(comprador.getSmartphones(),comprador.getTablets(),comprador.getTelevisions(),comprador.getLaptopss(),comprador.getPcss())
     }
-
-    public static Socio nuevo(Scanner sc){
-        int numeroDeCuenta;
-        String nombre;
-        String apellido;
-        String codigoDeConvenio;
-        System.out.print("Introduzca numero de cuenta: ");
-        numeroDeCuenta = sc.nextInt();
-        System.out.print("Introduzca nombre: ");
-        nombre = sc.nextLine();
-        System.out.print("Introduzca apellido: ");
-        apellido = sc.nextLine();
-        System.out.print("Introduzca cual es el codigo de convenio: ");
-        codigoDeConvenio = sc.nextLine();
-        return new Socio(nombre,apellido,numeroDeCuenta,codigoDeConvenio);
-    }
-    
-    protected void setCodigoDeConvenio(String codigoDeConvenio){
-        this.codigoDeConvenio=codigoDeConvenio;
-    }
-    
-    protected String getCodigoDeConvenio(){
-        return codigoDeConvenio;
+    public static Comprador iniciarSocio(){
+        LinkedList<Smartphone> smartphones= new LinkedList<>();
+        LinkedList<Tablet> tablets= new LinkedList<>();
+        LinkedList<Television> televisiones= new LinkedList<>();
+        LinkedList<Laptop> laptops= new LinkedList<>();
+        LinkedList<PC> pcs= new LinkedList<>();
+        Smartphone smartphone=new Smartphone(2020, 10000.0, "Huawei", "Mate 30", true, "Atandt","Plus","Proteccion sencilla", "media");
+        smartphones.add(smartphone);
+        Tablet tablet=new Tablet(2020,11000.0,"Samsung","Samsungpad",true,true);
+        tablets.add(tablet);
+        Television tele=new Television(2018, 9000.00,"Samsung Led", 2000, "Estereo");
+        televisiones.add(tele);
+        Laptop laptop=new Laptop(2018,15000.0, "Huawei notebook", "Huawei", 10, 400);
+        laptops.add(laptop);
+        PC pc=new PC(2019, 15000.0, "Samsung series A", "Samsung", true);
+        pcs.add(pc);
+        comprador=new Comprador(smartphones,tablets,televisiones,laptops,pcs);
+        return comprador;
     }
 }
